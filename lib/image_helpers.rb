a79ae498eca1c0dbbb616bd03210f2ab295f4981
Marco Zampetti <m.zampetti@cantierecreativo.net>
2020-12-16 14:47:31 +0100

    Remove unused helpers

--- lib/image_helpers.rb
+++ lib/image_helpers.rb
@@ -21,32 +21,4 @@ module ImageHelpers
   end
   alias_method :i, :icon
 
-  def image_lazy(image, url_options, sizes = [], attributes = {})
-    options = attributes.symbolize_keys
-    url_options.merge!({auto: 'format,compress'})
-
-    options[:title] ||= image.title
-    options[:alt] ||= image.alt
-
-    sizes_string = sizes.map do |width, size|
-      "#{image.url(url_options.merge!({w: width}))} #{size}w"
-    end
-    options.merge!({
-      data: {src: image.url(url_options), srcset: sizes_string.join(", ")}
-    })
-
-    image_tag(image.url(url_options.merge!({w: sizes.first.first})), options)
-  end
-
-  def image_tag(path, options_hash = {})
-    options = options_hash.dup
-    if !options.key?(:class)
-      options[:class] = "lazyload"
-    else
-      old_class = options.delete(:class)
-      options[:class] = "lazyload #{old_class}"
-    end
-
-    super(path, options)
-  end
 end
