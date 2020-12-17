816648b05af3dbe6973426a2ece26775a040ef8d
Matteo Manzo <m.manzo@cantierecreativo.net>
2020-12-17 15:50:45 +0100

    Dropdown on article aside

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,4 +1,5 @@
-// Placeholder
 //import Swiper from "swiper/bundle";
+import "jquery";
+import "@popperjs/core";
 import "bootstrap-italia/src/js/plugins/dropdown";
 import "bootstrap-italia/src/js/plugins/navbar";
