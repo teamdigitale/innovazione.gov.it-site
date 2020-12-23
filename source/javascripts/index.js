b4eb902dc908340769127074ed88c2e1a514cd14
Marco Zampetti <m.zampetti@cantierecreativo.net>
2020-12-23 14:20:43 +0100

    Import lazysizes library for responsive lazy images

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,5 +1,6 @@
 //import Swiper from "swiper/bundle";
-import 'bootstrap/dist/js/bootstrap';
-import 'bootstrap-select/js/bootstrap-select.js';
-import 'bootstrap-italia/src/js/plugins/dropdown';
-import 'bootstrap-italia/src/js/plugins/navbar';
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap-select/js/bootstrap-select.js";
+import "bootstrap-italia/src/js/plugins/dropdown";
+import "bootstrap-italia/src/js/plugins/navbar";
+import "lazysizes";
