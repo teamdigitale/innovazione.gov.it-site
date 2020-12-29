413766d0838a4d00308cf0c815ad4bf46fbb1911
Matteo Manzo <m.manzo@cantierecreativo.net>
2020-12-29 18:44:14 +0100

    Use swiper

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,6 +1,25 @@
-//import Swiper from "swiper/bundle";
+import Swiper from "swiper/bundle";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap-select/js/bootstrap-select.js";
 import "bootstrap-italia/src/js/plugins/dropdown";
 import "bootstrap-italia/src/js/plugins/navbar";
 import "lazysizes";
+
+var mySwiper = new Swiper ('.swiper-container', {
+  autoplay: {
+    delay: 5000,
+  },
+  init: false,
+  speed: 750,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
+
+mySwiper.init()
