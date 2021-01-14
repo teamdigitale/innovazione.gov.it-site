4659c3d282b7f64ac392c37f7aac1c0a59db8371
Matteo Manzo <m.manzo@cantierecreativo.net>
2021-01-14 16:55:16 +0100

    Handle progress bar on scroll

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -64,3 +64,12 @@ const initSearch = () => {
 };
 
 initSearch();
+
+window.onscroll = function() {progressIndicator()};
+
+function progressIndicator() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progress-indicator").style.width = scrolled + "%";
+}
