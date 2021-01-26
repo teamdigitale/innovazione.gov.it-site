b1d8b9ec834e13b43ac5b309c9856876bda81dd5
Matteo Manzo <m.manzo@cantierecreativo.net>
2021-01-26 11:22:44 +0100

    Close accordion on tap outside

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -68,6 +68,14 @@ const initSearch = () => {
   });
 };
 
+// Close accordion on click outside
+$(document).click(function(e) {
+  var container = $('.collapse');
+  if (!container.is(e.target) && container.has(e.target).length === 0) {
+    container.collapse('hide');
+  }
+})
+
 initSearch();
 progressIndicator.updateProgress()
 window.onscroll = function() {progressIndicator.updateProgress()};
