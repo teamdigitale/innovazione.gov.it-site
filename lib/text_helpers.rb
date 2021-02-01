179961b1f14fca085dbc1987619b1177bf47ee3e
Matteo Papadopoulos <m.papadopoulos@cantierecreativo.net>
2021-02-01 16:41:23 +0100

    Refactoring code of the external link functions

--- lib/text_helpers.rb
+++ lib/text_helpers.rb
@@ -5,62 +5,62 @@ module TextHelpers
 
   def markdown(content)
     return "" if content.blank?
-    
+
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::XHTML,
-      autolink: true, space_after_headers: true)
+                                       autolink: true, space_after_headers: true)
     new_content = markdown.render(content)
     replace_external_links(new_content)
   end
-  
+
   WORDS_PER_MINUTE = 180
-  
+
   def article_reading_time(page)
     return 0 if page.item_type.api_key != "article"
-    
+
     text_blocks = page.content_blocks.select do |block|
       block.item_type.api_key == "block_body_text"
     end
-    
+
     text = text_blocks.map do |block|
       [block.text_title, block.body_text].join(" ")
     end.join(" ")
-    
+
     reading_time(text)
   end
-  
+
   def reading_time(text)
     words = text.split.size
     (words / WORDS_PER_MINUTE).floor
   end
-  
+
   def replace_external_links(content)
     return "" if content.blank?
-    # 1. controllare tutti i tag a
-    # 2. Se il tag a continene un link a un dominio diverso da BASE_URL
-    # 3. Aggiungere un attributo target="_blank"
-    # 4. Se il link è == BASE_URL ma è un pdf
-    # 5. oltre a target, aggiungere un attributo aria-label="È un pdf"
 
     doc = Nokogiri::HTML(content)
-    
+
     links = doc.search("a")
     links.each do |link|
       url = link.attributes["href"].content
-      if !url.include? ENV["BASE_URL"]
-        link.set_attribute("target", "_blank")
-        link.set_attribute("rel", "noopener")
-        link.set_attribute("aria-label",
-          "#{link.content} (si apre in un nuovo tab)")
-      end
-      
-      if url.include? ".pdf"
-        link.set_attribute("target", "_blank")
-        link.set_attribute("rel", "noopener")
-        link.set_attribute("aria-label", "link a file PDF")
-        link.content = "#{link.content} (PDF)"
-      end
+      add_link_attributes(link) unless url.include? ENV["BASE_URL"]
+      add_pdf_attributes(link) if url.include? ".pdf"
     end
 
     doc.to_html
+  end
+
+  def add_link_attributes(link)
+    link.set_attribute("target", "_blank")
+    link.set_attribute("rel", "noopener")
+    link.set_attribute("aria-label",
+                       "#{link.content} (si apre in un nuovo tab)")
+    link
+  end
+
+  def add_pdf_attributes(link)
+    link.set_attribute("target", "_blank")
+    link.set_attribute("rel", "noopener")
+    link.set_attribute("aria-label", "link a file PDF")
+    link.content = "#{link.content} (PDF)"
+    link
   end
 end
