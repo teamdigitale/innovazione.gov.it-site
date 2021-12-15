5a089fa4b0371cec1e456f98f104b0cdb2548853
Natalia Baeza <baezanat@gmail.com>
2021-12-15 16:18:17 +0100

    Create migration for block_first_flag in homepage

--- migrations/1639494438_addContentToHomepageContentBlocks.js
+++ migrations/1639494438_addContentToHomepageContentBlocks.js
@@ -10,7 +10,7 @@ module.exports = async (client) => {
     }
   });
   const homepageContent = homepageContents[0]
-  const homepageRecordId = homepageContent["id"];
+  const homepageRecordId = homepageContent.id;
 
   // New blocks
   const focusBlock = await client.itemType.find('block_focus_highlight');
@@ -19,45 +19,59 @@ module.exports = async (client) => {
   const flagBlock = await client.itemType.find('block_first_flag');
 
   // Old content for minister block
-  const ministerPreTitle = homepageContent["ministerPreTitle"];
-  const ministerTitle = homepageContent["ministerTitle"];
-  const ministerText = homepageContent["ministerText"];
-  const ministerFeaturedNews = homepageContent["ministerFeaturedNews"];
+  const ministerPreTitle = homepageContent.ministerPreTitle;
+  const ministerTitle = homepageContent.ministerTitle;
+  const ministerText = homepageContent.ministerText;
+  const ministerFeaturedNews = homepageContent.ministerFeaturedNews;
 
   // Old content for Italy 2024 block
-  const italy2026PreTitle = homepageContent["italy2026PreTitle"];
-  const italy2026Title = homepageContent["italy2026Title"];
-  const italy2026Text = homepageContent["italy2026Text"];
-  const italy2026Thumbnail = homepageContent["italy2026Thumbnail"];
-  const italy2026Link = homepageContent["italy2026Link"];
-  const italy2026PercentNumbers = homepageContent["italy2026PercentNumbers"];
+  const italy2026PreTitle = homepageContent.italy2026PreTitle;
+  const italy2026Title = homepageContent.italy2026Title;
+  const italy2026Text = homepageContent.italy2026Text;
+  const italy2026Thumbnail = homepageContent.italy2026Thumbnail;
+  const italy2026Link = homepageContent.italy2026Link;
+  const italy2026PercentNumbers = homepageContent.italy2026PercentNumbers;
 
   //Old content for focus block
-  const focusElements = homepageContent["focusElements"];
+  const focusElements = homepageContent.focusElements;
+  const oldBlockFirstFlag = homepageContent.blockFirstFlags;
+  const oldBlockFirstFlagId = oldBlockFirstFlag[0];
+  const nestedRecord = await client.item.find(oldBlockFirstFlagId);
+  const firstFlagPreTitle = nestedRecord.preTitle;
+  const firstFlagTitle = nestedRecord.title;
+  const firstFlagImage = nestedRecord.image;
+  const firstFlagLink = nestedRecord.link;
 
   const createMinisterBlock = await client.items
     .update(homepageRecordId, {
       contentBlocks: [
         buildModularBlock({
-          itemType: ministerBlock.id,
-          title: ministerTitle,
-          preTitle: ministerPreTitle,
-          text: ministerText,
-          featuredInterviews: ministerFeaturedNews
-        }),
-        buildModularBlock({
           itemType: italy2026Block.id,
-          title: italy2026Title,
           preTitle: italy2026PreTitle,
+          title: italy2026Title,
           text: italy2026Text,
           thumbnail: italy2026Thumbnail,
           link: italy2026Link,
           percentNumbers: italy2026PercentNumbers
         }),
         buildModularBlock({
+          title: nestedRecord.title,
+          itemType: flagBlock.id,
+          preTitle: nestedRecord.preTitle,
+          image: nestedRecord.image,
+          link: nestedRecord.link,
+        }),
+        buildModularBlock({
+          itemType: ministerBlock.id,
+          preTitle: ministerPreTitle,
+          title: ministerTitle,
+          text: ministerText,
+          featuredInterviews: ministerFeaturedNews
+        }),
+        buildModularBlock({
           itemType: focusBlock.id,
           focusElements: focusElements,
-        })
+        }),
       ],
   });
 }
