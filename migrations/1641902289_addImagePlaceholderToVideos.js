e7bde838d92ca9bae7ffa3f8bb1ee8a4486850ab
Natalia Baeza <baezanat@gmail.com>
2022-01-11 13:02:15 +0100

    Add placeholder image and boolean fields to video model

---
+++ migrations/1641902289_addImagePlaceholderToVideos.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = async (client) => {
+  // Find video model
+  const videoModel = await client.itemType.find('video');
+
+  // Find content fieldset
+  const videoFieldsets = await client.fieldsets.all('video');
+  const contentFieldset = videoFieldsets.find(fieldset => fieldset.title === "Contenuto");
+
+  // Add image field to video model under content fieldset
+  const placeholderImgField = await client.fields.create(videoModel.id, {
+    label: 'Immagine di anteprima',
+    apiKey: 'placeholder_image',
+    fieldType: 'file',
+    fieldset: contentFieldset.id,
+    position: 3,
+    validators: {
+      extension: {
+        predefined_list: 'image',
+      },
+      required_alt_title: {
+        alt: true,
+        title: false
+      },
+    },
+    appearance: {
+      editor: 'file',
+      parameters: {},
+      addons: [],
+    },
+  });
+
+  // Add boolean to use placeholder image instead of youtube thumbnail
+  const usePlaceholderImgField = await client.fields.create(videoModel.id, {
+    label: 'Usare immagine di anteprima',
+    apiKey: 'use_placeholder_imagee',
+    fieldType: 'boolean',
+    fieldset: contentFieldset.id,
+    position: 4,
+    validators: {},
+    appearance: {
+      editor: 'boolean',
+      parameters: {},
+      addons: [],
+    },
+    hint: 'Usare imagine di anteprima invece della thumbnail caricata automaticamente da Youtube'
+  });
+}
