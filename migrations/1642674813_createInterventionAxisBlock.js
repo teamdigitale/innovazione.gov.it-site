3bd2e419668439c2dcb81ef7cf6ab9b207afd742
Natalia Baeza <baezanat@gmail.com>
2022-02-24 16:03:06 +0100

    Create intervention axes block

--- migrations/1642674813_createInterventionAxisBlock.js
+++ migrations/1642674813_createInterventionAxisBlock.js
@@ -1,26 +1,26 @@
-'use strict';
+"use strict";
 
 module.exports = async (client) => {
   // Create intervention axis block
   const interventionBlock = await client.itemTypes.create({
     name: "Blocco intervento",
-    apiKey: 'intervention_block',
+    apiKey: "intervention_block",
     modularBlock: true,
   });
 
   // Add title
   const titleField = await client.fields.create(interventionBlock.id, {
-    label: 'Titolo',
-    apiKey: 'title',
-    fieldType: 'string',
+    label: "Titolo",
+    apiKey: "title",
+    fieldType: "string",
     validators: {
       required: {},
       length: {
-        max: 120
+        max: 120,
       },
     },
     appearance: {
-      editor: 'single_line',
+      editor: "single_line",
       parameters: {
         heading: true,
       },
@@ -30,17 +30,17 @@ module.exports = async (client) => {
 
   // Add description
   const descriptionField = await client.fields.create(interventionBlock.id, {
-    label: 'Descrizione',
-    apiKey: 'description',
-    fieldType: 'string',
+    label: "Descrizione",
+    apiKey: "description",
+    fieldType: "string",
     validators: {
       required: {},
       length: {
-        max: 160
+        max: 180,
       },
     },
     appearance: {
-      editor: 'single_line',
+      editor: "single_line",
       parameters: {
         heading: false,
       },
@@ -50,27 +50,26 @@ module.exports = async (client) => {
 
   // Add image
   const thumbnailField = await client.fields.create(interventionBlock.id, {
-    label: 'Thumbnail',
-    apiKey: 'thumbnail',
-    fieldType: 'file',
+    label: "Thumbnail",
+    apiKey: "thumbnail",
+    fieldType: "file",
     validators: {
       file_size: {
         max_value: 1,
-        max_unit: 'MB',
+        max_unit: "MB",
       },
       extension: {
-        predefined_list: 'image',
+        predefined_list: "image",
       },
       required_alt_title: {
         alt: true,
-        title: false
+        title: false,
       },
     },
     appearance: {
-      editor: 'file',
+      editor: "file",
       parameters: {},
       addons: [],
     },
   });
-
-}
+};
