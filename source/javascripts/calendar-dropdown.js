f377d2e26183ed098e294714292b063394a1a2c7
Natalia Baeza <baezanat@gmail.com>
2022-03-14 18:55:54 +0100

    Adjust calendar block fading to go in tandem with navigation

--- source/javascripts/calendar-dropdown.js
+++ source/javascripts/calendar-dropdown.js
@@ -1,14 +1,6 @@
-// Change active title in dropdown menu when a calendar is clicked in the calendars block of Italy2026 page and hide from dropdown the option shown in title
+// Change active title in dropdown menu when a calendar is clicked in the calendars block of Italy2026 page
 
 $(".click-tab").click(function (event) {
-  //$(".click-tab")
-  //  .not(".chosen")
-  //  .each(function (index, el) {
-  //    $(this).show();
-  //  });
-  //$(".chosen").removeClass(".chosen");
   const value = $(this).text();
   $("#active-calendar").text(value);
-  //$(this).addClass(".chosen");
-  //$(this).hide();
 });
