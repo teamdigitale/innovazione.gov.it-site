7dc85aa78c278a2d3d3f50bec87bb1ffe9f5158b
Natalia Baeza <baezanat@gmail.com>
2022-03-14 18:55:54 +0100

    Adjust calendar block fading to go in tandem with navigation

--- source/javascripts/calendar-swiper.js
+++ source/javascripts/calendar-swiper.js
@@ -9,17 +9,14 @@ if ($(".swiper-container-calendar").length > 0) {
     const slides = $this.data("slides");
     const slidesNumber = slides < 4 ? slides : 4;
     const initial_slide_index = $this.data("initial");
-    console.log("initial slide index:");
-    console.log(initial_slide_index);
     $this.addClass("calendar-instance-" + index); //instance need to be unique
-    $this
-      .parent()
-      .find(".swiper-button-prev")
-      .addClass("prev-" + index); //prev must be unique
-    $this
-      .parent()
-      .find(".swiper-button-next")
-      .addClass("next-" + index); //next must be unique
+
+    // Remove fading effect where needed based on active slide
+    if (slides > 4) {
+      $this.addClass("_mid-faded-calendar");
+    }
+
+    // init calendars
     swiperInstances[index] = new Swiper(".calendar-instance-" + index, {
       //instance needs to be unique (ex: some-slider)
       observer: true,
@@ -49,3 +46,60 @@ if ($(".swiper-container-calendar").length > 0) {
     });
   });
 }
+
+$(".swiper-container-calendar").each(function (index, el) {
+  if ($(this).hasClass(`calendar-instance-${index}`)) {
+    $(`.prev-${index}`).click(function (e) {
+      const $activeEl = $(`.calendar-instance-${index}`)
+        .children()
+        .find(".swiper-slide-active");
+      const activeLabel = $activeEl.attr("aria-label");
+
+      if (typeof activeLabel !== "undefined") {
+        const slideMatch = activeLabel.match(/^(\d*)\s\/\s(\d*)/);
+        const activeSlide = parseInt(slideMatch[1]);
+        const totalSlides = parseInt(slideMatch[2]);
+        const firstSlideActive = activeSlide === 1;
+        const lastSlideShown = activeSlide === totalSlides - 3;
+
+        if (firstSlideActive) {
+          $(`.swiper-container-calendar.calendar-instance-${index}`)
+            .removeClass("_mid-faded-calendar")
+            .addClass("_mid-fade-out");
+        } else if (!lastSlideShown) {
+          $(`.swiper-container-calendar.calendar-instance-${index}`)
+            .removeClass("_mid-fade-in")
+            .removeClass("_mid-fade-out")
+            .addClass("_mid-faded-calendar");
+        }
+      }
+    });
+
+    $(`.next-${index}`).click(function (e) {
+      const $activeEl = $(`.calendar-instance-${index}`)
+        .children()
+        .find(".swiper-slide-active");
+      const activeLabel = $activeEl.attr("aria-label");
+
+      if (typeof activeLabel !== "undefined") {
+        const slideMatch = activeLabel.match(/^(\d*)\s\/\s(\d*)/);
+        const activeSlide = parseInt(slideMatch[1]);
+        const totalSlides = parseInt(slideMatch[2]);
+        const firstSlideActive = activeSlide === 1;
+        const lastSlideShown = activeSlide === totalSlides - 3;
+
+        if (lastSlideShown) {
+          $(`.swiper-container-calendar.calendar-instance-${index}`)
+            .removeClass("_mid-faded-calendar")
+            .removeClass("_mid-fade-out")
+            .addClass("_mid-fade-in");
+        } else if (!firstSlideActive) {
+          $(`.swiper-container-calendar.calendar-instance-${index}`)
+            .removeClass("_mid-fade-in")
+            .removeClass("_mid-fade-out")
+            .addClass("_mid-faded-calendar");
+        }
+      }
+    });
+  }
+});
