89d364956edb6bb2edf4a1573979bff95cf96cfc
Natalia Baeza <baezanat@gmail.com>
2022-03-15 15:29:22 +0100

    Hard code env variable for branch build

--- config.rb
+++ config.rb
@@ -52,8 +52,8 @@ retry_on_error(limit: 10) do
            token: ENV.fetch("DATO_API_TOKEN"),
            live_reload: true,
            preview: ENV.fetch("BUILD_ENV") != "production",
-           environment: "develop"
            #environment: DATO_ENV
+           environment: "develop"
 end
 
 webpack_command =
