9ec35b0112e56b631a8d1b4e510485f07a611bf8
Natalia Baeza <baezanat@gmail.com>
2022-03-16 17:59:14 +0100

    Style fading for desktop and mobile in calendar block

--- source/javascripts/calendar-swiper.js
+++ source/javascripts/calendar-swiper.js
@@ -1,20 +1,22 @@
 import Swiper from "swiper/bundle";
 
+const swiperInstances = [];
+
 if ($(".swiper-container-calendar").length > 0) {
   //some-slider-wrap-in
-  let swiperInstances = [];
+  //let swiperInstances = [];
   $(".swiper-container-calendar").each(function (index, element) {
     //some-slider-wrap-in
     const $this = $(this);
     const slides = $this.data("slides");
     const slidesNumber = slides < 4 ? slides : 4;
-    const initial_slide_index = $this.data("initial");
+    const initialSlideIndex = $this.data("initial");
     $this.addClass("calendar-instance-" + index); //instance need to be unique
 
     // Remove fading effect where needed based on active slide
-    if (slides > 4) {
-      $this.addClass("_mid-faded-calendar");
-    }
+    //if (slides > 4) {
+    //  $this.addClass("_mid-faded-calendar");
+    //}
 
     // init calendars
     swiperInstances[index] = new Swiper(".calendar-instance-" + index, {
@@ -24,7 +26,7 @@ if ($(".swiper-container-calendar").length > 0) {
       preloadImages: false,
       lazy: true,
       spaceBetween: 0,
-      initialSlide: initial_slide_index,
+      initialSlide: initialSlideIndex,
       breakpoints: {
         // medium is 768 and large is 992
         992: {
@@ -43,63 +45,80 @@ if ($(".swiper-container-calendar").length > 0) {
         firstSlideMessage: "Questa è la prima slide",
         lastSlideMessage: "Questa è l'ultima slide",
       },
+      on: {
+        init: function () {
+          console.log("swiper initialized");
+          let container = this.el;
+          let totalSlides = parseInt(container.dataset.slides);
+          let initialSlideIndex = parseInt(container.dataset.initial);
+          let initialSlideIsLast = initialSlideIndex === totalSlides - 1;
+
+          if (initialSlideIndex === 0 && totalSlides > 4) {
+            container.classList.add("_mid-fade-out", "_mid-mobile-fade-out");
+          } else if (totalSlides > 4 && initialSlideIsLast) {
+            container.classList.add("_mid-mobile-fade-in");
+          } else if (totalSlides > 4 && totalSlides - initialSlideIndex <= 4) {
+            container.classList.add(
+              "_mid-fade-in",
+              "_mid-mobile-faded-calendar"
+            );
+          } else if (totalSlides > 4) {
+            container.classList.add(
+              "_mid-faded-calendar",
+              "_mid-mobile-faded-calendar"
+            );
+          } else if (initialSlideIndex === 0) {
+            container.classList.add("_mid-mobile-fade-out");
+          } else if (initialSlideIsLast) {
+            container.classList.add("_mid-mobile-fade-in");
+          } else {
+            container.classList.add("_mid-mobile-faded-calendar");
+          }
+        },
+      },
     });
   });
 }
 
-$(".swiper-container-calendar").each(function (index, el) {
-  if ($(this).hasClass(`calendar-instance-${index}`)) {
-    $(`.prev-${index}`).click(function (e) {
-      const $activeEl = $(`.calendar-instance-${index}`)
-        .children()
-        .find(".swiper-slide-active");
-      const activeLabel = $activeEl.attr("aria-label");
+swiperInstances.forEach(function (swiper, index) {
+  const container = swiper.el;
 
-      if (typeof activeLabel !== "undefined") {
-        const slideMatch = activeLabel.match(/^(\d*)\s\/\s(\d*)/);
-        const activeSlide = parseInt(slideMatch[1]);
-        const totalSlides = parseInt(slideMatch[2]);
-        const firstSlideActive = activeSlide === 1;
-        const lastSlideShown = activeSlide === totalSlides - 3;
+  swiper.on("reachEnd", function () {
+    if (!swiper.isBeginning) {
+      container.classList.remove(
+        "_mid-faded-calendar",
+        "_mid-mobile-faded-calendar",
+        "_mid-fade-out",
+        "_mid-mobile-fade-out"
+      );
+      container.classList.add("_mid-fade-in", "_mid-mobile-fade-in");
+    }
+  });
 
-        if (firstSlideActive) {
-          $(`.swiper-container-calendar.calendar-instance-${index}`)
-            .removeClass("_mid-faded-calendar")
-            .addClass("_mid-fade-out");
-        } else if (!lastSlideShown) {
-          $(`.swiper-container-calendar.calendar-instance-${index}`)
-            .removeClass("_mid-fade-in")
-            .removeClass("_mid-fade-out")
-            .addClass("_mid-faded-calendar");
-        }
-      }
-    });
+  swiper.on("fromEdge", function () {
+    if (!(swiper.isBeginning || swiper.isEnd)) {
+      container.classList.remove(
+        "_mid-fade-in",
+        "_mid-mobile-fade-in",
+        "_mid-fade-out",
+        "_mid-mobile-fade-out"
+      );
+      container.classList.add(
+        "_mid-faded-calendar",
+        "_mid-mobile-faded-calendar"
+      );
+    }
+  });
 
-    $(`.next-${index}`).click(function (e) {
-      const $activeEl = $(`.calendar-instance-${index}`)
-        .children()
-        .find(".swiper-slide-active");
-      const activeLabel = $activeEl.attr("aria-label");
-
-      if (typeof activeLabel !== "undefined") {
-        const slideMatch = activeLabel.match(/^(\d*)\s\/\s(\d*)/);
-        const activeSlide = parseInt(slideMatch[1]);
-        const totalSlides = parseInt(slideMatch[2]);
-        const firstSlideActive = activeSlide === 1;
-        const lastSlideShown = activeSlide === totalSlides - 3;
-
-        if (lastSlideShown) {
-          $(`.swiper-container-calendar.calendar-instance-${index}`)
-            .removeClass("_mid-faded-calendar")
-            .removeClass("_mid-fade-out")
-            .addClass("_mid-fade-in");
-        } else if (!firstSlideActive) {
-          $(`.swiper-container-calendar.calendar-instance-${index}`)
-            .removeClass("_mid-fade-in")
-            .removeClass("_mid-fade-out")
-            .addClass("_mid-faded-calendar");
-        }
-      }
-    });
-  }
+  swiper.on("reachBeginning", function () {
+    if (!swiper.isEnd) {
+      container.classList.remove(
+        "_mid-faded-calendar",
+        "_mid-mobile-faded-calendar",
+        "_mid-fade-in",
+        "_mid-mobile-fade-in"
+      );
+      container.classList.add("_mid-fade-out", "_mid-mobile-fade-out");
+    }
+  });
 });
