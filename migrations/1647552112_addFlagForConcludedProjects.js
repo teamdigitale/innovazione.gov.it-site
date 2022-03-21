2f28906b3ef37f7cb04bf234442ce3f3a4f2488f
Natalia Baeza <baezanat@gmail.com>
2022-03-21 17:20:50 +0100

    Add completion date to notification in completed project show page

--- migrations/1647552112_addFlagForConcludedProjects.js
+++ migrations/1647552112_addFlagForConcludedProjects.js
@@ -32,7 +32,7 @@ module.exports = async (client) => {
     fieldset: stateFieldset.id,
     apiKey: "warning_text",
     fieldType: "string",
-    defaultValue: "Questo progetto è stato completato e non è piu attivo",
+    defaultValue: "Questo progetto è stato completato e non è piu attivo.",
     validators: {
       length: {
         max: 120,
