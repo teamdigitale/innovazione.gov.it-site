e0cc54144cae41ec730f5d49d8dc5ff32ac99858
Natalia Baeza <baezanat@gmail.com>
2022-09-19 12:25:50 +0200

    Clean installation of Bootstrap Italia 2.0

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,6 +1,6 @@
 import Swiper from "swiper/bundle";
 import "bootstrap/dist/js/bootstrap";
-import "bootstrap-select/js/bootstrap-select.js";
+//import "bootstrap-select/js/bootstrap-select.js";
 import "bootstrap-italia/src/js/plugins/dropdown";
 import "bootstrap-italia/src/js/plugins/navbar-collapsible";
 import "calendar-swiper.js";
