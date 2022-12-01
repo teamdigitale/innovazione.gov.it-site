023726214355f597d3f579013f87a57e57d488d6
Natalia Baeza <baezanat@gmail.com>
2022-12-01 13:25:25 +0100

    Add echarts and some graphs

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -10,9 +10,10 @@ import "lazysizes";
 import "lazysizes/plugins/respimg/ls.respimg";
 import "focus-visible/src/focus-visible.js";
 import Sharer from "sharer.js/sharer.js";
+import * as echarts from "echarts";
+
 const progressIndicator = require("progress-indicator.js");
 const DatoCmsSearch = require("datocms-search.widget.js");
-
 const searchClient = new DatoCmsSearch(
   "7bc02ea800b5526cd655912c1b6cfa",
   "production"
@@ -74,4 +75,245 @@ for (let index = 0; index < carouselCalendarList.length; index++) {
   const carousel = carouselCalendarList[index];
   const carouselInstances = [];
   carouselInstances[index] = new CarouselCalendar(carousel);
+}
+
+// Charts per dataviz
+const chartDom = document.getElementById("chart1");
+const chartDom2 = document.getElementById("chart2");
+const chartDom3 = document.getElementById("chart3");
+
+if (chartDom) {
+  var myChart = echarts.init(chartDom);
+  const labelOption = {
+    show: true,
+    // rotate: 90,
+    formatter: "{c}  {name|{a}}",
+    fontSize: 12,
+    rich: {
+      name: {},
+    },
+  };
+
+  const options = {
+    grid: { top: 20, right: 40, bottom: 20, left: 240 },
+    textStyle: {
+      fontFamily: "Roboto Mono",
+      fontWeight: "bold",
+      fontSize: 12,
+    },
+    yAxis: {
+      type: "category",
+
+      data: [
+        "Item 3",
+        "Item 4",
+        "Item 5",
+        "1.4.1 Esperienza del cittadino nei servizi pubblici",
+        "1.4.3 Adozione PagoPa e AppIo",
+      ],
+    },
+    xAxis: {
+      type: "value",
+    },
+
+    toolbox: {
+      show: true,
+      left: "center",
+      top: "top",
+      feature: {
+        dataView: {},
+        // restore: {},
+        saveAsImage: {},
+      },
+    },
+    series: [
+      {
+        name: "stanziati",
+        baseline: "bottom",
+        color: "#94c4f5",
+        data: [400, 500, 360, 200, 280],
+        type: "bar",
+        smooth: true,
+        barWidth: "20%",
+        label: labelOption,
+        itemStyle: {
+          borderRadius: [0, 10, 10, 0],
+        },
+      },
+      {
+        name: "spesi",
+        color: "#0066cc",
+        data: [280, 300, 350, 250, 320],
+        type: "bar",
+        smooth: true,
+        barWidth: "20%",
+        label: labelOption,
+        itemStyle: {
+          borderRadius: [0, 10, 10, 0],
+        },
+      },
+    ],
+    legend: {
+      left: "right",
+      top: "top",
+    },
+    tooltip: {},
+  };
+
+  options && myChart.setOption(options);
+}
+
+// Pie chart
+if (chartDom2) {
+  var myChart = echarts.init(chartDom2);
+  //PIE CHART
+  const options = {
+    color: ["#5c6f82", "#BFDFFF", "#207BD6", "#004D99", "#6AAAEB"],
+    textStyle: {
+      fontFamily: "Roboto Mono",
+      fontWeight: "bold",
+      fontSize: 10
+    },
+    tooltip: {},
+    legend: {
+      bottom: "5%",
+      left: "center"
+    },
+    title: {
+      text: `Total 47000\n 100%`,
+      left: "center",
+      top: "center"
+    },
+    toolbox: {
+      show: true,
+      left: "center",
+      top: "5%",
+      feature: {
+        dataView: {},
+        //restore: {},
+        saveAsImage: {}
+      }
+    },
+    series: [
+      {
+        name: "Access From",
+        type: "pie",
+        radius: ["40%", "70%"],
+        avoidLabelOverlap: false,
+        label: {
+          show: true,
+          position: "inside"
+        },
+        // emphasis: {
+        //   label: {
+        //     show: true,
+        //     fontSize: "40",
+        //     fontWeight: "bold"
+        //   }
+        // },
+        labelLine: {
+          show: false
+        },
+        data: [
+          { value: 1048, name: "Search Engine" },
+          { value: 735, name: "Direct" },
+          { value: 580, name: "Email" },
+          { value: 484, name: "Union Ads" },
+          { value: 300, name: "Video Ads" }
+        ]
+      }
+    ]
+  }
+
+  options && myChart.setOption(options);
+}
+
+if (chartDom3) {
+  var myChart = echarts.init(chartDom3);
+  //TREE-MAP
+  const options = {
+    textStyle: {
+      fontFamily: "monospace",
+      fontWeight: "normal",
+      fontSize: 12,
+    },
+    toolbox: {
+      show: true,
+      left: "center",
+      top: "top",
+      feature: {
+        dataView: {},
+        restore: {},
+        saveAsImage: {},
+      },
+    },
+    series: [
+      {
+        type: "treemap",
+        roam: false,
+        label: {
+          show: true,
+          formatter: "{b}",
+          normal: {
+            textStyle: {
+              ellipsis: true,
+            },
+          },
+        },
+        visualMin: -100,
+        visualMax: 100,
+        visualDimension: 3,
+        data: [
+          {
+            name: "Abilitazione cloud",
+            value: 34,
+            itemStyle: {
+              color: "#5c6f82",
+            },
+          },
+          {
+            name: "Adozione PagoPa e AppIo",
+            value: 32,
+            itemStyle: {
+              color: "#BFDFFF",
+            },
+          },
+          {
+            name: "Esperienza cittadino",
+            value: 26,
+            itemStyle: {
+              color: "#207BD6",
+            },
+          },
+          {
+            name: "Adozione identità digitale",
+            value: 7,
+            itemStyle: {
+              color: "#004D99",
+            },
+            children: [
+              {
+                name: "Adozione identità digitalo",
+                value: 5,
+              },
+              {
+                name: "Esperienza cittadino",
+                value: 2,
+                itemStyle: {
+                  color: "#6AAAEB",
+                },
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    legend: {
+      show: false,
+      left: "auto",
+      top: 0,
+    },
+    tooltip: {},
+  };
+  options && myChart.setOption(options);
 }
