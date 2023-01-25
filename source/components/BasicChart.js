e276edcd131746049ba0e310a24cc24830d813f0
Lorezz <point.point@gmail.com>
2023-01-25 17:11:51 +0100

    wip charts

--- source/components/BasicChart.js
+++ source/components/BasicChart.js
@@ -1,70 +1,75 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactEcharts from 'echarts-for-react';
 
-
-function BasicChart({ id, config ,dataSource }) {
+function BasicChart({ id, config, dataSource }) {
+  useEffect(() => {
+    console.log('MOUNT');
+    return () => {
+      console.log('UNMOUNT');
+    };
+  }, []);
   const axis =
     config.direction === 'vertical'
       ? {
-        dataZoom: [
-          {
-            show: config.zoom !== 'none',
-            realtime: true,
-            start: 0,
-            end: 100,
-            xAxisIndex: [0, 1],
-            type: config.zoom === 'slider' ? 'slider' : 'inside',
+          dataZoom: [
+            {
+              show: config.zoom !== 'none',
+              realtime: true,
+              start: 0,
+              end: 100,
+              xAxisIndex: [0, 1],
+              type: config.zoom === 'slider' ? 'slider' : 'inside',
+            },
+          ],
+          xAxis: {
+            type: 'category',
+            data: dataSource.categories,
+            axisTick: {
+              alignWithLabel: true,
+            },
+            axisLabel: {
+              rotate: 30,
+              inside: false,
+              // margin: 8,
+            },
           },
-        ],
-        xAxis: {
-          type: 'category',
-          data: dataSource.categories,
-          axisTick: {
-            alignWithLabel: true,
+          yAxis: {
+            type: 'value',
+            axisTick: {
+              alignWithLabel: true,
+            },
           },
-          axisLabel: {
-            rotate: 30,
-            inside: false,
-            // margin: 8,
-          },
-        },
-        yAxis: {
-          type: 'value',
-          axisTick: {
-            alignWithLabel: true,
-          },
-        },
-      }
+        }
       : {
-        dataZoom: [
-          {
-            show: config.zoom !== 'none',
-            realtime: true,
-            start: 0,
-            end: 100,
-            yAxisIndex: [0, 1],
-            type: config.zoom === 'slider' ? 'slider' : 'inside',
+          dataZoom: [
+            {
+              show: config.zoom !== 'none',
+              realtime: true,
+              start: 0,
+              end: 100,
+              yAxisIndex: [0, 1],
+              type: config.zoom === 'slider' ? 'slider' : 'inside',
+            },
+          ],
+          yAxis: {
+            type: 'category',
+            data: dataSource.categories,
+            axisTick: {
+              alignWithLabel: true,
+            },
           },
-        ],
-        yAxis: {
-          type: 'category',
-          data: dataSource.categories,
-          axisTick: {
-            alignWithLabel: true,
+          xAxis: {
+            type: 'value',
+            axisTick: {
+              alignWithLabel: true,
+            },
+            axisLabel: {
+              rotate: 90,
+              inside: true,
+              margin: 0,
+            },
           },
-        },
-        xAxis: {
-          type: 'value',
-          axisTick: {
-            alignWithLabel: true,
-          },
-          axisLabel: {
-            rotate: 90,
-            inside: true,
-            margin: 0,
-          },
-        },
-      };
+        };
 
   const options = {
     color: config.colors,
