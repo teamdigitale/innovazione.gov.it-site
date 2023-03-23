2e758b82c63992a58fdf28761c82f3bee680a5e8
Lorezz <point.point@gmail.com>
2023-03-23 18:33:37 +0100

    update charts

--- source/components/BasicChart.js
+++ source/components/BasicChart.js
@@ -1,14 +1,9 @@
-import React, { useEffect, useRef } from 'react';
-import ReactEcharts from 'echarts-for-react';
+import React, { useEffect, useRef } from "react";
+import ReactEcharts from "echarts-for-react";
+import { formatTooltip, log } from "./utils/chartUtils";
 
 function BasicChart({ id, data, setEchartInstance }) {
   const refCanvas = useRef(null);
-  // useEffect(() => {
-  //   console.log('MOUNT');
-  //   return () => {
-  //     console.log('UNMOUNT');
-  //   };
-  // }, []);
 
   useEffect(() => {
     if (refCanvas.current) {
@@ -20,23 +15,31 @@ function BasicChart({ id, data, setEchartInstance }) {
 
   function getOptions(data) {
     const config = data.config;
-    const zoom = config.zoom;
+    let grid = {
+      left: config.gridLeft || "10%",
+      right: config.gridRight || "10%",
+      height: config.gridHeight || "auto",
+      width: config.gridWidth || "auto",
+      bottom: config.gridBottom || 60,
+      top: config.gridTop || 60,
+    };
+    const zoom = config.zoom || "none";
     let dataZoom = [];
-    if (zoom !== 'none') {
+    if (zoom !== "none") {
       const x = [
         {
           show: true,
           start: 1,
           end: 100,
           xAxisIndex: [0],
-          type: 'inside',
+          type: "inside",
         },
         {
           show: true,
           start: 1,
           end: 100,
-          yAxisIndex: [0],
-          type: 'inside',
+          xAxisIndex: [0],
+          type: "slider",
         },
       ];
       const y = [
@@ -44,60 +47,59 @@ function BasicChart({ id, data, setEchartInstance }) {
           show: true,
           start: 1,
           end: 100,
-          xAxisIndex: [0],
-          type: 'slider',
+          yAxisIndex: [0],
+          type: "inside",
         },
         {
           show: true,
           start: 1,
           end: 100,
           yAxisIndex: [0],
-          type: 'slider',
+          type: "slider",
         },
       ];
 
-      if (zoom === 'both_axis') {
+      if (zoom === "both_axis") {
         dataZoom = [...x, ...y];
-      } else if (zoom === 'x_axis') {
+      } else if (zoom === "x_axis") {
         dataZoom = [...x];
-      } else if (zoom === 'y_axis') {
+      } else if (zoom === "y_axis") {
         dataZoom = [...y];
       }
     }
 
+    let dataZoomOpt = ["both_axis", "x_axis", "y_axis"].includes(zoom)
+      ? { dataZoom }
+      : {};
+
     let xName = config.xLabel
       ? {
           name: config.xLabel,
-          nameLocation: 'middle',
-          nameGap: 30,
+          nameLocation: "middle",
+          nameGap: 50,
         }
       : {};
     let yName = config.yLabel
       ? {
           name: config.yLabel,
-          nameLocation: 'middle',
-          nameGap: 30,
+          nameLocation: "middle",
+          nameGap: 50,
         }
       : {};
 
     const axis =
-      config.direction === 'vertical'
+      config.direction === "vertical"
         ? {
             xAxis: {
               ...xName,
-              type: 'category',
+              type: "category",
               data: data.dataSource.categories,
               alignTicks: true,
-              // axisLabel: {
-              //   rotate: 30,
-              //   inside: false,
-              //   // margin: 8,
-              // },
             },
             yAxis: {
               ...yName,
               nameRotate: 90,
-              type: 'value',
+              type: "value",
               alignTicks: true,
             },
           }
@@ -105,104 +107,81 @@ function BasicChart({ id, data, setEchartInstance }) {
             yAxis: {
               ...xName,
               nameRotate: 90,
-              type: 'category',
+              type: "category",
               data: data.dataSource.categories,
               alignTicks: true,
             },
             xAxis: {
               ...yName,
-              type: 'value',
+              type: "value",
               alignTicks: true,
-              // axisLabel: {
-              //   rotate: 90,
-              //   inside: true,
-              //   margin: 0,
-              // },
             },
           };
 
     const tooltip = {
-      trigger: config.tooltipTrigger || 'item',
+      trigger: config.tooltipTrigger || "item",
       axisPointer: {
-        type: config.axisPointer,
+        type: "shadow",
       },
       valueFormatter: (value) => {
-        const formatter = config.tooltipFormatter;
-        const valueFormatter = config.valueFormatter;
-        let valueFormatted = value;
-        if (formatter) {
-          if (formatter === 'percentage') {
-            valueFormatted = `${value}%`;
-          } else if (formatter === 'currency') {
-            valueFormatted = new Intl.NumberFormat('it-IT', {
-              style: 'currency',
-              currency: 'EUR',
-            }).format(value);
-          } else if (formatter === 'number') {
-            valueFormatted = new Intl.NumberFormat('it-IT', {
-              style: 'decimal',
-            }).format(value);
-          }
-        }
-        return `${valueFormatted} ${valueFormatter ? valueFormatter : ''}`;
+        return formatTooltip(value, config);
       },
       show: config.tooltip,
-      // formatter: (params) => {},
     };
 
     const options = {
-      backgroundColor: config.background ? config.background : '#F2F7FC',
-      color: config.colors,
+      backgroundColor: config.background ? config.background : "#F2F7FC",
+      color: config.colors || null,
       ...axis,
+      grid,
       series: data.dataSource.series.map((serie) => {
         let rest = { stack: false, smooth: false };
         if (config.stack) {
           let stack = config.stack
-            ? config.direction === 'vertical'
-              ? 'x'
-              : 'y'
+            ? config.direction === "vertical"
+              ? "x"
+              : "y"
             : false;
           rest = { ...rest, stack };
         }
-        if (serie.type === 'line' && config.smooth) {
+        if (serie.type === "line" && config.smooth) {
           let smooth = config.smooth ? parseFloat(config.smooth) : false;
           rest = { ...rest, smooth };
         }
-
-        // console.log('rest', rest);
         return {
           ...serie,
           ...rest,
         };
       }),
       textStyle: {
-        fontFamily: 'Titillium Web, sans-serif',
-        fontWeight: 'semibold',
+        fontFamily: "Titillium Web, sans-serif",
+        fontWeight: "semibold",
         fontSize: 12,
       },
       tooltip,
       legend: {
-        left: 'center',
-        top: 'tbottomp',
+        type: "scroll",
+        left: "center",
+        top: "bottom",
         show: config.legend,
       },
-      dataZoom,
+      ...dataZoomOpt,
     };
     return options;
   }
 
   if (!data) return <div>...</div>;
-  const chartHeight = data.config?.h || '500px';
+  const chartHeight = data.config?.h || "350px";
   return (
-    <div key={id} id={'chart_' + id}>
+    <div key={id} id={"chart_" + id}>
       <ReactEcharts
         id={id}
         option={getOptions(data)}
         ref={refCanvas}
         style={{
-          width: '100%',
+          width: "100%",
           height: chartHeight,
-          maxWidth: '100%',
+          maxWidth: "100%",
         }}
       />
     </div>
--- source/components/ChartWrapper.js
+++ source/components/ChartWrapper.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import BasicChart from './BasicChart';
-import PieChart from './PieChart';
-import GeoMapChart from './GeoMapChart';
-import Table from './Table';
+import React from "react";
+import BasicChart from "./BasicChart";
+import PieChart from "./PieChart";
+import GeoMapChart from "./GeoMapChart";
+// import Table from "./Table";
+import DataTable from "./DataTable";
+import { log } from "./utils/chartUtils";
 
 import {
-  generateCSV,
-  generateCSVPie,
+  dataToCSV,
   downLoadPng,
   downloadCSV,
   getPieValues,
-  getBarValues,
-  getLineValues,
+  getBasicValues,
   getMapValues,
-} from './utils/chartUtils';
+} from "./utils/chartUtils";
 
 export default function ChartWrapper(props) {
   const {
@@ -29,17 +29,12 @@ export default function ChartWrapper(props) {
     istance,
   } = props;
 
-  const { dataSource, config, chart } = data;
+  const { config, chart } = data;
   const [echartInstance, setEchartInstance] = React.useState(null);
 
-  const chartType = chart; //series.type;
-  const series = Array.isArray(dataSource.series)
-    ? dataSource.series[0]
-    : dataSource.series;
-  const csvData =
-    chartType === 'pie' ? generateCSVPie(series) : generateCSV(dataSource);
+  const chartType = chart;
+  const csvData = dataToCSV(data.data);
 
-  // console.log('csvData', csvData);
   return (
     <div className="px-3 pt-3 px-md-4 pt-md-4">
       <h3 className="mid-caption--lead fw-semibold text-black">{title}</h3>
@@ -49,7 +44,7 @@ export default function ChartWrapper(props) {
         id="myTab"
         role="tablist"
       >
-        {['Grafico', 'Tabella dati', 'Info'].map((name, i) => (
+        {["Grafico", "Tabella dati", "Info"].map((name, i) => (
           <li
             key={`${id}-tab_${i}-${istance}`}
             className="nav-item lightgrey-bg-a3"
@@ -58,7 +53,7 @@ export default function ChartWrapper(props) {
             <a
               aria-controls={`tab${i + 1}-${id}-content-${istance}`}
               aria-selected="true"
-              className={`nav-link ${i === 0 ? 'active' : ''}`}
+              className={`nav-link ${i === 0 ? "active" : ""}`}
               data-bs-toggle="tab"
               href={`#tab${i + 1}-${id}-content-${istance}`}
               id={`tab${i + 1}-${id}`}
@@ -73,7 +68,7 @@ export default function ChartWrapper(props) {
         <div
           aria-labelledby={`tab1-${id}-${istance}`}
           className="tab-pane mid-tabs-pane my-4 fade show active"
-          style={{ height: config.h ? config.h : '500px' }}
+          style={{ height: config.h ? config.h : "500px" }}
           id={`tab1-${id}-content-${istance}`}
           role="tabpanel"
         >
@@ -81,30 +76,23 @@ export default function ChartWrapper(props) {
           <div
             key={id}
             className="mid-chart"
-            style={{ height: config.h ? config.h : '500px' }}
+            style={{ height: config.h ? config.h : "500px" }}
           >
-            {chartType === 'bar' && (
+            {(chartType === "bar" || chartType === "line") && (
               <BasicChart
                 id={id}
-                data={getBarValues(data)}
+                data={getBasicValues(data)}
                 setEchartInstance={setEchartInstance}
               />
             )}
-            {chartType === 'line' && (
-              <BasicChart
-                id={id}
-                data={getLineValues(data)}
-                setEchartInstance={setEchartInstance}
-              />
-            )}
-            {chartType === 'pie' && (
+            {chartType === "pie" && (
               <PieChart
                 id={id}
                 data={getPieValues(data)}
                 setEchartInstance={setEchartInstance}
               />
             )}
-            {chartType === 'map' && (
+            {chartType === "map" && (
               <GeoMapChart
                 id={id}
                 data={getMapValues(data)}
@@ -119,7 +107,8 @@ export default function ChartWrapper(props) {
           id={`tab2-${id}-content-${istance}`}
           role="tabpanel"
         >
-          <Table id={id} ds={dataSource} />
+          {/* <Table id={id} ds={dataSource} /> */}
+          <DataTable id={id} data={data.data} />
         </div>
         <div
           aria-labelledby={`tab3-${id}-${istance}`}
@@ -127,19 +116,19 @@ export default function ChartWrapper(props) {
           id={`tab3-${id}-content-${istance}`}
           role="tabpanel"
         >
-          <div dangerouslySetInnerHTML={{ __html: `${info || ' '}` }} />
+          <div dangerouslySetInnerHTML={{ __html: `${info || " "}` }} />
         </div>
       </div>
       <div className="d-md-flex justify-content-md-between">
         <div className="pt-3">
           <span className="fw-semibold text-uppercase">
-            {labelsSource || 'Fonte dati'}:
+            {labelsSource || "Fonte dati"}:
           </span>
           <a
             href={source}
             className="ms-2 fw-semibold"
             target="_blank"
-            aria-label={labelsSource || 'Fonte dati'}
+            aria-label={labelsSource || "Fonte dati"}
           >
             {source}
           </a>
@@ -148,15 +137,15 @@ export default function ChartWrapper(props) {
           <span className="ps-md-2 pe-3 pe-md-0 pt-3 pb-md-0 fw-bold text-primary">
             <a
               className="mid-button-link"
-              title={labelsDownload || 'Scarica CSV'}
-              aria-label={labelsDownload || 'Scarica CSV'}
+              title={labelsDownload || "Scarica CSV"}
+              aria-label={labelsDownload || "Scarica CSV"}
               onClick={() => downloadCSV(csvData, id)}
             >
-              {labelsDownload || 'Scarica'} CSV
+              {labelsDownload || "Scarica"} CSV
               <svg
                 className="icon icon-sm icon-primary ms-1"
                 focusable="false"
-                aria-label={`${labelsDownload || 'Scarica'} CSV`}
+                aria-label={`${labelsDownload || "Scarica"} CSV`}
                 role="img"
               >
                 <use href="/images/sprite.svg#it-download"></use>
@@ -166,15 +155,15 @@ export default function ChartWrapper(props) {
           <span className="ps-md-2 pe-3 pe-md-0 pt-3 pb-md-0 fw-bold text-primary">
             <button
               className="mid-button-link"
-              title={labelsDownload || 'Scarica PNG'}
-              aria-label={labelsDownload || 'Scarica PNG'}
+              title={labelsDownload || "Scarica PNG"}
+              aria-label={labelsDownload || "Scarica PNG"}
               onClick={() => downLoadPng(echartInstance, id)}
             >
-              {labelsDownload || 'Scarica'} PNG
+              {labelsDownload || "Scarica"} PNG
               <svg
                 className="icon icon-sm icon-primary ms-1"
                 focusable="false"
-                aria-label={`${labelsDownload || 'Scarica'} PNG`}
+                aria-label={`${labelsDownload || "Scarica"} PNG`}
                 role="img"
               >
                 <use href="/images/sprite.svg#it-download"></use>
@@ -184,14 +173,14 @@ export default function ChartWrapper(props) {
           <span className="ps-md-2 pt-3 fw-bold text-primary">
             <button
               className="mid-button-link"
-              title={labelsShare || 'Condividi'}
-              aria-label={labelsShare || 'Condividi'}
+              title={labelsShare || "Condividi"}
+              aria-label={labelsShare || "Condividi"}
             >
-              {labelsShare || 'Condividi'}
+              {labelsShare || "Condividi"}
               <svg
                 className="icon icon-sm icon-primary ms-1"
                 focusable="false"
-                aria-label={labelsShare || 'Condividi'}
+                aria-label={labelsShare || "Condividi"}
                 role="img"
               >
                 <use href="/images/sprite.svg#it-share"></use>
